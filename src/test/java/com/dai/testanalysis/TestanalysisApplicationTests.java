39dc294faa7bed046c4a006048820d8dc6b2c330
dai <dongqtai@163.com>
2023-07-24 00:18:02 +0800

    first commit

---
+++ src/test/java/com/dai/testanalysis/TestanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai.testanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestanalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
